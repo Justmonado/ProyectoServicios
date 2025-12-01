0b6012fe4ad771c80276a0d841deafdf18f9b467
Alfonso Soots <alfonso_soots@hotmail.com>
2025-11-30 20:32:36 -0600

    consistencia middleware

--- Middleware/errorHandler.js
+++ Middleware/errorHandler.js
@@ -6,11 +6,13 @@ function logErrors(err, req, res, next) {
 function errorHandler(err, req, res, next) {
   if (err.message.includes('no encontrado') || err.message.includes('no encontrada')) {
     return res.status(404).json({
+      success:false,
       message: err.message
     });
   }
   
   res.status(500).json({
+    success:false,
     message: err.message
   });
 }
